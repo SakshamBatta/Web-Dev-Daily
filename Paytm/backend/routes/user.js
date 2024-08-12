ec899dec9aabc6d6417f00d718548a5d640353d1
Saksham Batta <sakshambatta20@gmail.com>
2024-08-13 00:24:52 +0530

    Added frontend code

--- Paytm/backend/routes/user.js
+++ Paytm/backend/routes/user.js
@@ -115,7 +115,7 @@ router.put("/update", authMiddleware, async (req, res) => {
 });
 
 router.get("/bulk", async (req, res) => {
-  const filter = req.query.filter || " ";
+  const filter = req.query.filter || "";
   const users = await User.find({
     $or: [
       {
